4d85d7003ead9b439dea7f22ec2af98e59e5d0eb
Estherjuma <153985400+Estherjuma@users.noreply.github.com>
2024-02-16 16:18:20 +0300

    Initialized project using github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1 className="App-header">Weather app by esther</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://github.com/Estherjuma" target="_blank">
+            Esther Juma
+          </a>
+           and is{" "}
+          <a href="" target="_blank">
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
